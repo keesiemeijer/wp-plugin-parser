28e745b9a6e4cedde15e1779b64c7ad24f828374
Kees Meijer <keesie.meijer@gmail.com>
2018-05-21 12:20:04 +0200

    Remove ajax transients on uninstall

--- uninstall.php
+++ uninstall.php
@@ -15,9 +15,15 @@ if ( is_multisite() ) {
 		foreach ( (array) $blogs as $blog ) {
 			switch_to_blog( $blog['blog_id'] );
 			delete_option( 'wp_plugin_parser_settings' );
+			delete_transient( 'wp_plugin_parser_ajax' );
+			delete_transient( 'wp_plugin_parser_ajax_uses' );
+			delete_transient( 'wp_plugin_parser_ajax_wp_uses' );
 		}
 		restore_current_blog();
 	}
 } else {
 	delete_option( 'wp_plugin_parser_settings' );
+	delete_transient( 'wp_plugin_parser_ajax' );
+	delete_transient( 'wp_plugin_parser_ajax_uses' );
+	delete_transient( 'wp_plugin_parser_ajax_wp_uses' );
 }
