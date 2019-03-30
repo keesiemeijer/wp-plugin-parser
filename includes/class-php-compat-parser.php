cec341d2d6bafd58869008c2c41990647bbe5ba4
Kees Meijer <keesie.meijer@gmail.com>
2019-03-30 18:13:56 +0100

    update dependencies

--- includes/class-php-compat-parser.php
+++ includes/class-php-compat-parser.php
@@ -1,6 +1,16 @@
 <?php
 namespace keesiemeijer\WP_Plugin_Parser;
 
+// We need to use the PHP_CS autoloader to access the Runner and Config.
+require_once WP_PLUGIN_PARSER_PLUGIN_DIR . '/vendor/squizlabs/php_codesniffer/autoload.php';
+require_once WP_PLUGIN_PARSER_PLUGIN_DIR . '/vendor/phpcompatibility/php-compatibility/PHPCompatibility/PHPCSHelper.php';
+
+use \PHP_CodeSniffer\Runner;
+use \PHP_CodeSniffer\Config;
+use \PHP_CodeSniffer\Reporter;
+use \PHP_CodeSniffer\Files\DummyFile;
+use \PHPCompatibility\PHPCSHelper;
+
 class PHP_Compat_Parser {
 
 	public $compat;
@@ -30,16 +40,6 @@ class PHP_Compat_Parser {
 	 *                                            False if an error occured.
 	 */
 	private function parse_compatibility( $files, $version ) {
-		if ( ! class_exists( 'PHPCompatibility\PHPCSHelper' ) ) {
-			$this->logger->log( __( 'PHPCompatibility sniffs not installed', 'wp-plugin-parser' ), 'compat' );
-			return false;
-		}
-
-		if ( ! class_exists( 'PHP_CodeSniffer_CLI' ) ) {
-			$this->logger->log( __( 'PHP CodeSniffer not installed', 'wp-plugin-parser' ), 'compat' );
-			return false;
-		}
-
 		$php_versions = get_php_versions();
 		if ( ! in_array( $version, $php_versions ) ) {
 			$this->logger->log( __( 'Invalid PHP version used for compatibility check', 'wp-plugin-parser' ), 'compat' );
@@ -59,12 +59,50 @@ class PHP_Compat_Parser {
 			'extensions'  => array( 'php' ),
 		);
 
-		call_user_func( array( 'PHPCompatibility\PHPCSHelper', 'setConfigData' ), 'testVersion', $args['testVersion'], true );
+		$runner = new Runner();
 
-		$cli  = new \PHP_CodeSniffer_CLI();
+		$config_args = ['-s', '-p', '-n'];
+
+		$runner->config = new Config( $config_args );
+
+		$runner->config->extensions = array(
+			'php' => 'PHP',
+			'inc' => 'PHP',
+		);
+
+		$ignored = '.*/node_modules/.*,.*/vendor/.*,.*/assets/build/.*,.*/build/.*,.*/bin/.*';
+
+
+		$runner->config->standards   = array( 'PHPCompatibility' );
+		$runner->config->files       = $files;
+		$runner->config->annotations = true;
+		$runner->config->parallel    = 8;
+		$runner->config->colors      = false;
+		$runner->config->tabWidth    = 0;
+		$runner->config->reportWidth = 110;
+		$runner->config->interactive = false;
+		$runner->config->cache       = false;
+		$runner->config->ignored     = $ignored;
+
+		$runner->init();
+
+		// Ignoring warnings when generating the exit code.
+		PHPCSHelper::setConfigData( 'ignore_warnings_on_exit', true, true );
+
+		// Set minimum supported PHP version.
+		PHPCSHelper::setConfigData( 'testVersion', $version , true );
+
+		$runner->reporter = new Reporter( $runner->config );
+
+		foreach ( $files as $file_path ) {
+			$file       = new DummyFile( file_get_contents( $file_path ), $runner->ruleset, $runner->config );
+			$file->path = $file_path;
+
+			$runner->processFile( $file );
+		}
 
 		ob_start();
-		$cli->process( $args );
+		$runner->reporter->printReports();
 		$report = ob_get_clean();
 
 		return $report;
